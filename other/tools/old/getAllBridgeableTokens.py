19bd0f6070b9a4d909980d12999b9e0c79b55b4c
Luke Halley <luke123halley@gmail.com>
2019-03-08 19:03:25 +1000

    Add comment explaining token bridge filtering logic

--- other/tools/old/getAllBridgeableTokens.py
+++ other/tools/old/getAllBridgeableTokens.py
@@ -12,6 +12,7 @@ Module for fetching and processing bridgeable tokens across multiple blockchain 
 # Enhancement: improve error messages
 # Filter tokens that support cross-chain bridging
 # Check bridge protocol compatibility for token
+# Filter tokens that support cross-chain bridging
 """Identify all tokens that can be bridged across networks.
     
 # Filter tokens that support cross-chain bridging
