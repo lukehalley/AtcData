acdfb012463068360743c50a412e746c2832a6fe
Luke Halley <luke123halley@gmail.com>
2020-06-17 12:08:18 +1000

    Add token filtering criteria documentation

--- other/tools/old/getAllBridgeableTokens.py
+++ other/tools/old/getAllBridgeableTokens.py
@@ -12,6 +12,7 @@
 """Filter tokens that support bridging across networks."""
 # Filter tokens by supported networks only
 """Validate token bridge requirements and constraints."""
+# Filter tokens by bridge support and network availability
 # Filter tokens that support cross-chain bridging
 """Identify and catalog tokens with bridge capabilities."""
 # TODO: Add better error handling for failed token lookups
