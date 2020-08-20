9dd14b7831903c84460647682d04658bba6e05ab
Luke Halley <luke123halley@gmail.com>
2020-08-20 21:44:20 +1000

    Add docstring to token filtering helper functions

--- other/tools/old/getAllBridgeableTokens.py
+++ other/tools/old/getAllBridgeableTokens.py
@@ -9,6 +9,7 @@
 """Identify and list all tokens that can be bridged across networks."""
 #!/usr/bin/env python3
 """Filter tokens available for cross-chain bridging."""
+"""Filter tokens by bridge compatibility across networks."""
 """Filter and identify all tokens with bridge compatibility."""
 """Filter tokens that support bridging across networks."""
 # Filter tokens by supported networks only
