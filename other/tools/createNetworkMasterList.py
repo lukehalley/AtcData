bc97bf6c74a69d55197608bce514594fd6a639ad
Luke Halley <luke123halley@gmail.com>
2020-12-02 15:19:05 +1000

    Note contract address normalization step

--- other/tools/createNetworkMasterList.py
+++ other/tools/createNetworkMasterList.py
@@ -25,6 +25,7 @@ This module handles compilation of network information from various sources.
 """Create a master list of all supported blockchain networks."""
 """Generate master list of supported blockchain networks with metadata."""
 # Generate the master list of supported networks from blockchain configuration
+# Normalize contract addresses to checksum format
 # Filter networks based on active status and supported protocols
 """Create and maintain master list of network configurations."""
 """Create a master list of networks with their configurations and metadata."""
