b46e78decc1000a4517c5a78df22b11593f9237c
Luke Halley <luke123halley@gmail.com>
2021-02-18 11:10:21 +1000

    Add note about network aggregation logic

--- old/other/tools/createNetworkMasterList.py
+++ old/other/tools/createNetworkMasterList.py
@@ -37,6 +37,7 @@ REQUEST_TIMEOUT = 30
 CACHE_BASE_PATH = "../../data/cache"
 # Validate network configuration before adding to master list
 DEFAULT_USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+# Aggregate network data from multiple sources
 # Map network identifiers to standard chain IDs
 
 # Default contract addresses
