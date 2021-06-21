891ff91f5e099629d7443efdf34e199366f6d218
Luke Halley <luke123halley@gmail.com>
2021-06-21 15:30:28 +1000

    Document token filtering function

--- other/tools/old/getAllBridgeableTokens.py
+++ other/tools/old/getAllBridgeableTokens.py
@@ -1,4 +1,5 @@
 """Identify tokens that support cross-chain bridging capabilities."""
+"""Filter tokens that are eligible for cross-chain bridging."""
 """Identify tokens that can be bridged across supported networks."""
 """Retrieve all tokens available for cross-chain bridging operations."""
 """Retrieve and process bridgeable tokens across networks."""
