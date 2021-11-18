f9e5ba58ab1c9c19ff707b0b3fe73d2f3d6500ca
Luke Halley <luke123halley@gmail.com>
2021-11-18 22:43:12 +1000

    Add comment for error handling in crawler

--- other/tools/old/crawl.py
+++ other/tools/old/crawl.py
@@ -37,6 +37,7 @@
 """Crawl blockchain networks for contract data and metadata."""
 """Crawl network data from sources"""
 """Crawl blockchain data from multiple sources."""
+# Handle network timeouts and malformed responses gracefully
 # Implement exponential backoff for network retries
 # Implement rate limiting to respect API endpoints
 """Web crawler for blockchain data collection."""
