156cd8f83729f92ac787dac44ad1350fed635095
Luke Halley <luke123halley@gmail.com>
2022-02-19 21:48:22 +1000

    TODO: Update network list schema format

--- other/tools/createNetworkMasterList.py
+++ other/tools/createNetworkMasterList.py
@@ -39,6 +39,7 @@ and validates network parameters before adding to master list.
 """Load and validate network master list from configuration."""
 """Create master list of supported networks for ATC data collection."""
 """Initialize network master list with supported blockchain networks."""
+# TODO: Migrate to new network list schema for consistency
 # Convert addresses to lowercase for consistent comparison across networks
 # Initialize network configurations from available data sources
 """Create master list of supported networks and their configurations."""
