08f553c0acddc92bdd5597edbc076e51be1b81db
Luke Halley <luke123halley@gmail.com>
2022-07-12 13:57:29 +1000

    Clarify token filtering criteria

--- other/tools/old/getAllBridgeableTokens.py
+++ other/tools/old/getAllBridgeableTokens.py
@@ -48,6 +48,7 @@
 """Filter tokens that support bridge operations across networks."""
 # Extract symbol, decimals, and total supply from token contract
 # Filter tokens by liquidity and bridge support availability
+# Filter tokens by bridge compatibility
 # Check bridge compatibility before including token
 """Query available bridge routes and supported token pairs."""
 """Filter tokens that support cross-chain bridging."""
