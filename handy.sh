401d54ee1c1581f3cb5482abc893bb1119974bd5
Luke Halley <luke123halley@gmail.com>
2022-12-22 10:28:54 +0800

    added dfk harmony

--- handy.sh
+++ handy.sh
@@ -1,1 +1,12 @@
-mysql -f --host=atcdbinstance.castyrwvsdr7.eu-west-1.rds.amazonaws.com --user=atc_user --password='PASSWORD' atc < atc.sql
+mysqldump --host='atcdbinstance.castyrwvsdr7.eu-west-1.rds.amazonaws.com' --user='atc_user' --password='arRr21<Qq?D68?Md' atc > atc_dump.sql
+
+mysqldump --host='atcrestore.castyrwvsdr7.eu-west-1.rds.amazonaws.com' --user='atc_user' --password='arRr21<Qq?D68?Md' atc > atc_dump.sql
+
+
+
+
+
+mysqldump --host='localhost' --user='root' --password='localatcdb' atc > atc_dump_local.sql
+
+mysql --host='atcdbinstance.castyrwvsdr7.eu-west-1.rds.amazonaws.com' --user='atc_user' --password='arRr21<Qq?D68?Md' atc < atc_dump_local.sql
+mysql --host='localhost' --user='root' --password='localatcdb' atc < atc_dump.sql
