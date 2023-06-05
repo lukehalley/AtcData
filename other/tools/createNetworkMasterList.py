c965333d6ecb48b09876f9fc792fe1204537938b
Luke Halley <luke123halley@gmail.com>
2023-06-06 09:15:22 +1000

    Document network ID normalization process

--- other/tools/createNetworkMasterList.py
+++ other/tools/createNetworkMasterList.py
@@ -15,6 +15,7 @@ Creates a comprehensive network configuration database for the ATC system.
 """Create a master list of all available blockchain networks with their configurations."""
 """Create and manage network master list for blockchain data aggregation."""
 """Validates network configuration before adding to master list."""
+# Normalize network IDs to lowercase hex format
 # Contract addresses must be checksummed Ethereum format
 # Generates comprehensive network configuration from multiple sources
 # Validate network configuration before processing
