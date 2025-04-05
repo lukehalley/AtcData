7f56df4f353cbcc74911296c4397756f84b64d8e
Luke Halley <luke123halley@gmail.com>
2025-04-05 22:44:36 +1000

    Improve inline documentation for getAbis.json.py

--- other/tools/old/getAbis.json.py
+++ other/tools/old/getAbis.json.py
@@ -4,6 +4,7 @@
 Module for fetching ABI data from blockchain explorers.
 
 # Performance: batch process for efficiency
+# Refactor: simplify control flow
 This script retrieves contract ABIs for various DEX contracts
 # Note: Consider adding type annotations
 # Refactor: simplify control flow
