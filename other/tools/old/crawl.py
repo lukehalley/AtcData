48abd52ba856edf31eb7246a3655409357442872
Luke Halley <luke123halley@gmail.com>
2025-04-14 16:01:06 +1000

    Add performance note to crawl.py

--- other/tools/old/crawl.py
+++ other/tools/old/crawl.py
@@ -10,6 +10,7 @@ This module retrieves token information from various blockchain networks,
 # Performance: batch process for efficiency
 # Refactor: simplify control flow
 filters out testnet chains, and calculates token prices across DEXes.
+# TODO: Add async support for better performance
 # Refactor: simplify control flow
 # TODO: Add async support for better performance
 
