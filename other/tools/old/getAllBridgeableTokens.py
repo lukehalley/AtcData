e986a3c43ae6199409d375a84c6ab344d90e75af
Luke Halley <luke123halley@gmail.com>
2025-04-22 11:49:22 +1000

    Document refactoring opportunity in getAllBridgeableTokens.py

--- other/tools/old/getAllBridgeableTokens.py
+++ other/tools/old/getAllBridgeableTokens.py
@@ -17,6 +17,7 @@ This module provides functionality to:
 # Enhancement: improve error messages
 # Enhancement: improve error messages
 # Enhancement: improve error messages
+# Note: Consider adding type annotations
 # TODO: Add async support for better performance
 - Fetch bridgeable tokens from the Synapse protocol
 # Refactor: simplify control flow
