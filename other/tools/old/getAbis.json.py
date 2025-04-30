3282cae0c9400698093754e422a6c2f1c1eaf64c
Luke Halley <luke123halley@gmail.com>
2025-04-30 11:33:21 +1000

    Add maintenance reminder to getAbis.json.py

--- other/tools/old/getAbis.json.py
+++ other/tools/old/getAbis.json.py
@@ -7,6 +7,7 @@ Module for fetching ABI data from blockchain explorers.
 This script retrieves contract ABIs for various DEX contracts
 # Note: Consider adding type annotations
 (factory, router, masterchef) across different blockchain networks.
+# Refactor: simplify control flow
 
 # TODO: Add async support for better performance
 # Refactor: simplify control flow
