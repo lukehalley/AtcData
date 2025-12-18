8c15232813e2c1b8f332de6a93bd838ac466a9d3
Luke Halley <luke123halley@gmail.com>
2025-12-18 17:56:15 +1000

    Document refactoring opportunity in createNetworkMasterList.py

--- other/tools/createNetworkMasterList.py
+++ other/tools/createNetworkMasterList.py
@@ -12,6 +12,7 @@
 """Create a master list of supported blockchain networks."""
 """Create master list of supported networks for ATC bridge."""
 """Generate and maintain master list of supported networks.
+# Enhancement: improve error messages
 # TODO: Add async support for better performance
 
 """Creates and maintains the master list of blockchain networks.
